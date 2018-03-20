7419769dd1e08251ddb9b646dc63936b71380e0e
Mauricio Nuñez <user@Mauricios-MacBook-Air.local>
2018-03-20 15:55:19 -0500

    New update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  <head>
 
   <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
-  <meta name="generator" content="2017.0.1.363"/>
+  <meta name="generator" content="2017.0.4.363"/>
   
   <script type="text/javascript">
    // Update the 'nojs'/'js' class on the html node
@@ -16,19 +16,17 @@ if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required
   <title>Home</title>
   <!-- CSS -->
   <link rel="stylesheet" type="text/css" href="css/site_global.css?crc=443350757"/>
-  <link rel="stylesheet" type="text/css" href="css/index.css?crc=47664348" id="pagesheet"/>
+  <link rel="stylesheet" type="text/css" href="css/index.css?crc=95249313" id="pagesheet"/>
   <!-- IE-only CSS -->
   <!--[if lt IE 9]>
-  <link rel="stylesheet" type="text/css" href="css/iefonts_index.css?crc=3778814228"/>
+  <link rel="stylesheet" type="text/css" href="css/iefonts_index.css?crc=164517508"/>
   <![endif]-->
   <!-- Other scripts -->
   <script type="text/javascript">
    var __adobewebfontsappname__ = "muse";
 </script>
   <!-- JS includes -->
-  <script type="text/javascript">
-   document.write('\x3Cscript src="' + (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//webfonts.creativecloud.com/lato:n4,n3,n7:default.js" type="text/javascript">\x3C/script>');
-</script>
+  <script src="https://webfonts.creativecloud.com/lato:n4,n3,n7:default.js" type="text/javascript"></script>
    </head>
  <body>
 
@@ -38,23 +36,15 @@ if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required
      <div class="browser_width grpelem" id="u94-bw">
       <div class="museBGSize" id="u94"><!-- group -->
        <div class="clearfix" id="u94_align_to_page">
-        <div class="clip_frame grpelem" id="u146"><!-- svg -->
-         <img class="svg" id="u142" src="images/mau-logo.svg?crc=97443492" width="66" height="30" alt="" data-mu-svgfallback="images/mau%20logo_poster_.png?crc=57748039"/>
-        </div>
+        <a class="nonblock nontext MuseLinkActive clip_frame grpelem" id="u146" href="index.html"><!-- svg --><img class="svg" id="u142" src="images/mau-logo.svg?crc=97443492" width="66" height="30" alt="" data-mu-svgfallback="images/mau%20logo_poster_.png?crc=57748039"/></a>
         <div class="clearfix grpelem" id="u105"><!-- group -->
          <div class="gradient rounded-corners grpelem" id="u136"><!-- content --></div>
         </div>
         <div class="clearfix grpelem" id="pu415"><!-- column -->
          <div class="clearfix colelem" id="u415"><!-- group -->
-          <div class="clearfix grpelem" id="u406-4"><!-- content -->
-           <p>Home</p>
-          </div>
-          <div class="clearfix grpelem" id="u409-4"><!-- content -->
-           <p>About Me</p>
-          </div>
-          <div class="clearfix grpelem" id="u412-4"><!-- content -->
-           <p>Contact</p>
-          </div>
+          <a class="nonblock nontext MuseLinkActive clearfix grpelem" id="u406-4" href="index.html"><!-- content --><p>Home</p></a>
+          <a class="nonblock nontext anim_swing clearfix grpelem" id="u409-4" href="index.html#about"><!-- content --><p>About Me</p></a>
+          <a class="nonblock nontext anim_swing clearfix grpelem" id="u412-4" href="index.html#contact"><!-- content --><p>Contact</p></a>
          </div>
          <div class="clearfix colelem" id="u133"><!-- column -->
           <div class="clearfix colelem" id="u116-6"><!-- content -->
@@ -70,12 +60,13 @@ if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required
       </div>
      </div>
      <div class="shadow clearfix grpelem" id="u139"><!-- column -->
+      <a class="anchor_item colelem" id="about"></a>
       <div class="clearfix colelem" id="u153-5"><!-- content -->
        <p><span id="u153">UI designer &amp; illustrator</span> located in Barranquilla, Colombia. Currently working as Junior UI Designer</p>
       </div>
       <div class="colelem" id="u156"><!-- simple frame --></div>
       <div class="clearfix colelem" id="u159-6"><!-- content -->
-       <p>My name is Maurico Núñez, 26 yeards old, Colombian. I have 4 years of experience working as a graphic designer/ illustrator in Sonovista Publicidad S.A. and 2 years working as a Junior UI designer in ideaware.co my currently job.</p>
+       <p>My name is Maurico Núñez, 26 yeards old, Colombian. I have 4 years of experience working as a graphic designer/ illustrator in Sonovista Publicidad S.A. and 2 years working as a Junior UI designer in ideaware.co my current job.</p>
        <p>I dedicate my free time for freelancing, drawing and videogaming.</p>
       </div>
      </div>
@@ -96,13 +87,11 @@ if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required
      <p><span id="u184">UI designer &amp; illustrator</span> located in Barranquilla, Colombia. Currently working as Junior UI Designer &amp; illustrator</p>
     </div>
     <div class="colelem" id="u183"><!-- simple frame --></div>
-    <div class="clearfix colelem" id="u194-4"><!-- content -->
-     <p>Glimpse Analytix</p>
-    </div>
+    <a class="nonblock nontext clearfix colelem" id="u194-4" href="glipsy.html"><!-- content --><p>Glimpse Analytix</p></a>
     <div class="clearfix colelem" id="u200-4"><!-- content -->
      <p>A software for psychiatrists and patients.</p>
     </div>
-    <div class="museBGSize colelem" id="u203"><!-- simple frame --></div>
+    <a class="nonblock nontext museBGSize colelem" id="u203" href="glipsy.html"><!-- simple frame --></a>
     <div class="clip_frame mse_pre_init" id="u303"><!-- svg -->
      <img class="svg" id="u299" src="images/bola2.svg?crc=444871524" width="342" height="615" alt="" data-mu-svgfallback="images/bola2_poster_.png?crc=4047870353"/>
     </div>
@@ -112,7 +101,7 @@ if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required
     <div class="clearfix colelem" id="u214-4"><!-- content -->
      <p>A software for organize your task.</p>
     </div>
-    <div class="museBGSize colelem" id="u220"><!-- simple frame --></div>
+    <a class="nonblock nontext museBGSize colelem" id="u220" href="upteam.html"><!-- simple frame --></a>
     <div class="clip_frame mse_pre_init" id="u297"><!-- svg -->
      <img class="svg" id="u293" src="images/bola1.svg?crc=4035965516" width="342" height="398" alt="" data-mu-svgfallback="images/bola1_poster_.png?crc=12767595"/>
     </div>
@@ -122,14 +111,14 @@ if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required
     <div class="clearfix colelem" id="u226-4"><!-- content -->
      <p>Landing header design and creation of illustrations.</p>
     </div>
-    <div class="museBGSize colelem" id="u223"><!-- simple frame --></div>
+    <a class="nonblock nontext museBGSize colelem" id="u223" href="ideaware.html"><!-- simple frame --></a>
     <div class="clearfix colelem" id="u232-4"><!-- content -->
      <p>InfographiQ</p>
     </div>
     <div class="clearfix colelem" id="u235-4"><!-- content -->
      <p>Infographics created for infographiQ, a software where clients &amp; designers work togheter to create infografics</p>
     </div>
-    <div class="museBGSize colelem" id="u238"><!-- simple frame --></div>
+    <a class="nonblock nontext museBGSize colelem" id="u238" href="infographiq.html"><!-- simple frame --></a>
     <div class="clip_frame mse_pre_init" id="u324"><!-- svg -->
      <img class="svg" id="u325" src="images/bola2.svg?crc=444871524" width="342" height="615" alt="" data-mu-svgfallback="images/bola2_poster_.png?crc=4047870353"/>
     </div>
@@ -139,24 +128,21 @@ if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required
     <div class="clearfix colelem" id="u244-4"><!-- content -->
      <p>Empty states design for multiple clients.</p>
     </div>
-    <div class="museBGSize colelem" id="u247"><!-- simple frame --></div>
+    <div class="clearfix colelem" id="pu247"><!-- group -->
+     <a class="nonblock nontext museBGSize grpelem" id="u247" href="multiple-clients.html"><!-- simple frame --></a>
+     <a class="anchor_item grpelem" id="contact"></a>
+    </div>
     <div class="clip_frame mse_pre_init" id="u315"><!-- svg -->
      <img class="svg" id="u316" src="images/bola1.svg?crc=4035965516" width="342" height="398" alt="" data-mu-svgfallback="images/bola1_poster_.png?crc=12767595"/>
     </div>
-    <div class="clearfix colelem" id="u333-6"><!-- content -->
+    <div class="clearfix colelem" id="u333-7"><!-- content -->
      <p id="u333-2">Do you like my work? You can contact me at</p>
-     <p id="u333-4">mauteve@gmail.com</p>
+     <p id="u333-5"><a class="nonblock" href="mailto:mauteve@gmail.com"><span id="u333-3">mauteve@gmail.com</span></a></p>
     </div>
     <div class="clearfix colelem" id="pu346"><!-- group -->
-     <div class="clip_frame grpelem" id="u346"><!-- svg -->
-      <img class="svg" id="u342" src="images/instagram.svg?crc=3971606391" width="42" height="42" alt="" data-mu-svgfallback="images/instagram_poster_.png?crc=4207622089"/>
-     </div>
-     <div class="clip_frame grpelem" id="u340"><!-- svg -->
-      <img class="svg" id="u336" src="images/facebook.svg?crc=303448346" width="42" height="42" alt="" data-mu-svgfallback="images/facebook_poster_.png?crc=166941972"/>
-     </div>
-     <div class="clip_frame grpelem" id="u352"><!-- svg -->
-      <img class="svg" id="u348" src="images/linkedin.svg?crc=256106211" width="42" height="42" alt="" data-mu-svgfallback="images/linkedin_poster_.png?crc=142197019"/>
-     </div>
+     <a class="nonblock nontext clip_frame grpelem" id="u346" href="https://www.instagram.com/mauteve/" target="_blank"><!-- svg --><img class="svg" id="u342" src="images/instagram.svg?crc=3971606391" width="42" height="42" alt="" data-mu-svgfallback="images/instagram_poster_.png?crc=4207622089"/></a>
+     <a class="nonblock nontext clip_frame grpelem" id="u340" href="https://www.facebook.com/mauricio.nunez.790" target="_blank"><!-- svg --><img class="svg" id="u336" src="images/facebook.svg?crc=303448346" width="42" height="42" alt="" data-mu-svgfallback="images/facebook_poster_.png?crc=166941972"/></a>
+     <a class="nonblock nontext clip_frame grpelem" id="u352" href="https://www.linkedin.com/in/mauricio-nu%C3%B1ez-03b488127/" target="_blank"><!-- svg --><img class="svg" id="u348" src="images/linkedin.svg?crc=256106211" width="42" height="42" alt="" data-mu-svgfallback="images/linkedin_poster_.png?crc=142197019"/></a>
     </div>
     <div class="verticalspacer" data-offset-top="5588" data-content-above-spacer="5587" data-content-below-spacer="279"></div>
     <div class="browser_width colelem" id="u375-bw">
@@ -178,10 +164,15 @@ if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required
   </div>
   <!-- Other scripts -->
   <script type="text/javascript">
+   // Decide weather to suppress missing file error or not based on preference setting
+var suppressMissingFileError = false
+</script>
+  <script type="text/javascript">
    window.Muse.assets.check=function(d){if(!window.Muse.assets.checked){window.Muse.assets.checked=!0;var b={},c=function(a,b){if(window.getComputedStyle){var c=window.getComputedStyle(a,null);return c&&c.getPropertyValue(b)||c&&c[b]||""}if(document.documentElement.currentStyle)return(c=a.currentStyle)&&c[b]||a.style&&a.style[b]||"";return""},a=function(a){if(a.match(/^rgb/))return a=a.replace(/\s+/g,"").match(/([\d\,]+)/gi)[0].split(","),(parseInt(a[0])<<16)+(parseInt(a[1])<<8)+parseInt(a[2]);if(a.match(/^\#/))return parseInt(a.substr(1),
 16);return 0},g=function(g){for(var f=document.getElementsByTagName("link"),h=0;h<f.length;h++)if("text/css"==f[h].type){var i=(f[h].href||"").match(/\/?css\/([\w\-]+\.css)\?crc=(\d+)/);if(!i||!i[1]||!i[2])break;b[i[1]]=i[2]}f=document.createElement("div");f.className="version";f.style.cssText="display:none; width:1px; height:1px;";document.getElementsByTagName("body")[0].appendChild(f);for(h=0;h<Muse.assets.required.length;){var i=Muse.assets.required[h],l=i.match(/([\w\-\.]+)\.(\w+)$/),k=l&&l[1]?
 l[1]:null,l=l&&l[2]?l[2]:null;switch(l.toLowerCase()){case "css":k=k.replace(/\W/gi,"_").replace(/^([^a-z])/gi,"_$1");f.className+=" "+k;k=a(c(f,"color"));l=a(c(f,"backgroundColor"));k!=0||l!=0?(Muse.assets.required.splice(h,1),"undefined"!=typeof b[i]&&(k!=b[i]>>>24||l!=(b[i]&16777215))&&Muse.assets.outOfDate.push(i)):h++;f.className="version";break;case "js":h++;break;default:throw Error("Unsupported file type: "+l);}}d?d().jquery!="1.8.3"&&Muse.assets.outOfDate.push("jquery-1.8.3.min.js"):Muse.assets.required.push("jquery-1.8.3.min.js");
-f.parentNode.removeChild(f);if(Muse.assets.outOfDate.length||Muse.assets.required.length)f="Some files on the server may be missing or incorrect. Clear browser cache and try again. If the problem persists please contact website author.",g&&Muse.assets.outOfDate.length&&(f+="\nOut of date: "+Muse.assets.outOfDate.join(",")),g&&Muse.assets.required.length&&(f+="\nMissing: "+Muse.assets.required.join(",")),alert(f)};location&&location.search&&location.search.match&&location.search.match(/muse_debug/gi)?setTimeout(function(){g(!0)},5E3):g()}};
+f.parentNode.removeChild(f);if(Muse.assets.outOfDate.length||Muse.assets.required.length)f="Some files on the server may be missing or incorrect. Clear browser cache and try again. If the problem persists please contact website author.",g&&Muse.assets.outOfDate.length&&(f+="\nOut of date: "+Muse.assets.outOfDate.join(",")),g&&Muse.assets.required.length&&(f+="\nMissing: "+Muse.assets.required.join(",")),suppressMissingFileError?(f+="\nUse SuppressMissingFileError key in AppPrefs.xml to show missing file error pop up.",console.log(f)):alert(f)};location&&location.search&&location.search.match&&location.search.match(/muse_debug/gi)?
+setTimeout(function(){g(!0)},5E3):g()}};
 var muse_init=function(){require.config({baseUrl:""});require(["jquery","museutils","whatinput","jquery.musepolyfill.bgsize","jquery.watch","jquery.scrolleffects"],function(d){var $ = d;$(document).ready(function(){try{
 window.Muse.assets.check($);/* body */
 Muse.Utils.transformMarkupToFixBrowserProblemsPreInit();/* body */
@@ -200,6 +191,6 @@ Muse.Utils.transformMarkupToFixBrowserProblems();/* body */
 
 </script>
   <!-- RequireJS script -->
-  <script src="scripts/require.js?crc=4159430777" type="text/javascript" async data-main="scripts/museconfig.js?crc=4179431180" onload="if (requirejs) requirejs.onError = function(requireType, requireModule) { if (requireType && requireType.toString && requireType.toString().indexOf && 0 <= requireType.toString().indexOf('#scripterror')) window.Muse.assets.check(); }" onerror="window.Muse.assets.check();"></script>
+  <script src="scripts/require.js?crc=3951022091" type="text/javascript" async data-main="scripts/museconfig.js?crc=4177022356" onload="if (requirejs) requirejs.onError = function(requireType, requireModule) { if (requireType && requireType.toString && requireType.toString().indexOf && 0 <= requireType.toString().indexOf('#scripterror')) window.Muse.assets.check(); }" onerror="window.Muse.assets.check();"></script>
    </body>
 </html>
